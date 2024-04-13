da598ba1dfb1701b22ded519d3794ede7d51ab31
SrhDP <>
2024-04-13 10:54:10 +0700

    create audit

--- loans/src/main/java/com/example/loans/LoansApplication.java
+++ loans/src/main/java/com/example/loans/LoansApplication.java
@@ -2,8 +2,13 @@ package com.example.loans;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+/*@ComponentScans({ @ComponentScan("com.eazybytes.loans.controller") })
+@EnableJpaRepositories("com.eazybytes.loans.repository")
+@EntityScan("com.eazybytes.loans.model")*/
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 public class LoansApplication {
 
 	public static void main(String[] args) {
